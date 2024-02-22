2c821aadfc77417fecd690284ea4a183dfd08d9d
kyaakpa <100368545+kyaakpa@users.noreply.github.com>
2024-02-23 04:36:51 +1100

    navbar improvements

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,16 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import MainNav from "@/components/MainNav";
 import Footer from "@/components/Footer";
 import FormModal from "@/components/FormModal";
 import Home from "./page";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light min-w-[300px]">
-      <body className={inter.className}>
+      <body>
         <Providers>
           <MainNav />
-          <FormModal styling="max-lg:fixed max-lg:right-4 max-lg:bottom-4 lg:hidden z-50" />
           {children}
           <Footer />
         </Providers>
