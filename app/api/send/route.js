3c2b9eb4721f61dfced1823351c6b719bb1c1186
kyaakpa <100368545+kyaakpa@users.noreply.github.com>
2024-05-04 17:39:39 +1000

    api fix

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -22,7 +22,7 @@ export async function POST(req) {
   try {
     await resend.emails.send({
       from: `${firstName + " " + lastName} <onboarding@resend.dev>`,
-      to: ["kyaakpalama@webtion.org"],
+      to: ["Markjaherninc@gmail.com"],
       subject: "We have received your message",
       react: (
         <Email
