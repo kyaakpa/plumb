a18e14414ec0675f55402c2795f379c96b25ea29
kenma-zks <pallawmgr20@gmail.com>
2024-05-04 13:55:25 +0545

    trying nodemailer

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,33 +1,42 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import { Email } from "./email";
+import nodemailer from "nodemailer";
 
 export async function POST(req) {
   const form = await req.json();
-  const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
   const firstName = form.firstName;
   const lastName = form.lastName;
   const description = form.description;
   const phone = form.phone;
   const email = form.email;
+
   try {
-    await resend.emails.send({
-      from: `${firstName + " " + lastName} <onboarding@resend.dev>`,
-      to: ["kyaakpalama@webtion.org"],
-      subject: "We have received your message",
-      react: (
-        <Email
-          firstName={firstName}
-          lastName={lastName}
-          description={description}
-          phone={phone}
-          email={email}
-          updatedDate={new Date()}
-        />
-      ),
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "pallaw020@gmail.com",
+        pass: "Deathend124",
+      },
     });
+
+    // Define the email options
+    const mailOptions = {
+      from: `${firstName + " " + lastName} <your-email@example.com>`,
+      to: "kyaakpalama@gmail.com",
+      subject: "We have received your message",
+      text: `
+        Name: ${firstName} ${lastName}
+        Description: ${description}
+        Phone: ${phone}
+        Email: ${email}
+        Date: ${new Date()}
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
     return NextResponse.json({ message: "ok" });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ message: "failed" });
   }
 }
